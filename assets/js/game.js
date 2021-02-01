6044661ce82232f2f5e05100b0617d42a5175678
tristan <westenskowtristan122@gmail.com>
2021-02-01 15:11:09 -0700

    adding playerName prompt in Js

--- assets/js/game.js
+++ assets/js/game.js
@@ -1,1 +1,14 @@
-window.alert("This is an alert!");
+function fight() {
+  window.alert("The fight had begun!");
+}
+//fight();
+
+var playerName = window.prompt("What is your robot's name?");
+//note the lack of quotations marks around playerName
+console.log(playerName);
+
+console.log("This logs a string, good for leaving yourself a message");
+//This will do math and log 20
+console.log(10 + 10);
+//what is this?
+console.log("Our robot's name is " + playerName);
